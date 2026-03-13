c6d59034e37e44aba1768a0dc8e760fb3587bef2
Kileab <kileab@naver.com>
2026-03-13 20:48:32 +0900

    (2026.3.13) 인터프리터

---
+++ Class1th (Interpreter).py
@@ -0,0 +1,9 @@
+# 인터프리터 : 프로그램의 소스 코드를 한 줄씩 읽어서 바로 실행하는 프로그램입니다.
+
+print("Congratulations on learning Python") 
+
+print("Let's start fresh.")
+
+# 모듈(Module) : 특정 기능을 수행하기 위해 독립적으로 구성되어 있는 파일입니다.
+
+# 터미널(Terminal) : 컴퓨터에게 명령어를 직접 입력해서 실행하는 화면입니다.
