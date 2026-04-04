cb4fb64019cf7a24a082e12ec97fc1600a8005ca
Kileab <kileab@naver.com>
2026-04-04 20:34:20 +0900

    (2026.4.4) 변수

---
+++ Class3th (Variable).py
@@ -0,0 +1,42 @@
+# region 변수
+# 객체를 참조하기 위한 이름으로, 해당 객체의 메모리 위치를 가리킵니다.
+
+switch = False
+
+size = 10
+
+friction = 0.5
+
+message = "File not found."
+
+print("switch :", switch)
+
+print("size :", size)
+
+print("friction :", friction)
+
+print("message :", message)
+
+
+# f-string : 문자열 앞에 f 또는 F를 붙이고, { } 안에 변수나 표현식을 넣으면 그 값이 문자열에 삽입됩니다.
+
+# region 변수의 이름 규칙
+# 1. 변수의 이름은 대소문자를 구분합니다.
+# ex) health = 10
+# ex) HEALTH = 20
+
+# 2. 변수의 이름으로 예약어를 사용할 수 없습니다.
+# ex) if = 3.141592
+
+# 3. 변수의 이름에는 공백이 포함될 수 없습니다.
+# ex) time delta = 0.016
+
+# 4. 변수의 이름으로 특수 문자는 "_"만 사용할 수 있습니다.
+# version_1 = 1
+
+# 5. 변수의 이름은 숫자로 시작할 수 없습니다.
+# 3day = "Wednesday"
+# ex) month9 = "september" 
+# endregion
+
+# endregion
